8d2bba0e5115fa40efc32bac7007aa642a8ac751
Alexandre-petitjean <petitjean.alexandre.pro@gmail.com>
2018-12-13 13:56:30 +0100

    mon premier formulaire symfony

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -4,16 +4,26 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class LoginController extends AbstractController
-{
+class LoginController extends AbstractController {
+
     /**
      * @Route("/login", name="login")
      */
-    public function index()
-    {
-        return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
-        ]);
+    public function index() {
+        $formLogin = $this->createFormLogin();
+        return $this->render('login/index.html.twig', array('formLogin' => $formLogin->createView()));
     }
+
+    private function createFormLogin() {
+        return $this->createFormBuilder()
+                        ->add('mailAddress', EmailType::class)
+                        ->add('password', PasswordType::class)
+                        ->add('submit', SubmitType::class)
+                        ->getForm();
+    }
+
 }
